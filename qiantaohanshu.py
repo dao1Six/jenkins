f38124fdf67412e280ddf35bccae55372a221565
wujianlun <wujianlun@do1.com.cn>
2019-11-01 10:08:18 +0800

    完善班车构建功能

--- qiantaohanshu.py
+++ qiantaohanshu.py
@@ -1,13 +1,18 @@
-class A():
+# 包含yield关键字，就变成了生成器函数
+# 调用函数并不会执行语句
+def foo():
+    print('Starting.....')
+    while True:
+        res = yield 4
+        print("res:", res)
 
-    @classmethod
-    def outer(cls):
-        x = 1
-        def inner():
-            y = x + 1
-            print (y)
-        print(inner)
+# 下面调用函数并没有执行，可以先将后面的语句注释掉
+# 逐行运行代码观察效果
+g = foo()
+print("第一次调用执行结果：")
+print(next(g))
+print("*" * 100)
 
-
-if __name__ == '__main__':
-    A.outer()
+print("第二次调用执行结果：")
+print(next(g))
+print("*" * 100)
