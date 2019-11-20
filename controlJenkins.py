da89bc0c312707235bc5164bf26025d06843440c
wujianlun <wujianlun@do1.com.cn>
2019-11-20 18:32:33 +0800

    完善班车构建功能

--- controlJenkins.py
+++ controlJenkins.py
@@ -29,17 +29,15 @@ class ContrlJenlins:
 
 
 if __name__ == '__main__':
-    # jobList = ["七巧-后端/bpms-repository","七巧-后端/fdn-message","七巧-后端/fdn-authorize","七巧-后端/fdn-storage","七巧-后端/fdn-schema","七巧-前端/console","七巧-前端/official-web","七巧-前端/official-mobile","七巧-前端/m-runtime","七巧-后端/bpms-workbench", "七巧-前端/runtime","七巧-前端/operation","七巧-前端/help","七巧-后端/bpms-runtime","七巧-后端/bpms-operation","七巧-后端/bpms-portal","七巧-后端/bpms-appstore"]
-    # qajenkins = {'url':'http://jenkins.qa.do1.work','username':'qiqiao','password':'1llR0lbA'}
-    # uatjenkins = {'url':'http://jenkins.uat.do1.work','username':'do1','password':'7HWoxn8Q'}
-    prodjenkins = {'url': 'http://jenkins.qiqiao-prod.do1.work', 'username': 'qiqiao', 'password': '6LypDvN2'}
+    jobList = ["七巧-后端/bpms-repository","七巧-后端/fdn-message","七巧-后端/fdn-authorize","七巧-后端/fdn-storage","七巧-后端/fdn-schema","七巧-前端/console","七巧-前端/official-web","七巧-前端/official-mobile","七巧-前端/m-runtime","七巧-后端/bpms-workbench", "七巧-前端/runtime","七巧-前端/operation","七巧-前端/help","七巧-后端/bpms-runtime","七巧-后端/bpms-operation","七巧-后端/bpms-portal","七巧-后端/bpms-appstore"]
+    qajenkins = {'url':'http://jenkins.qa.do1.work','username':'qiqiao','password':'1llR0lbA'}
+    uatjenkins = {'url':'http://jenkins.uat.do1.work','username':'do1','password':'7HWoxn8Q'}
+    param_dict = {'Branch':'origin/fixbug-1.1.6','PublishVersion':'true'}
+    execute_environment = uatjenkins
+    a = ContrlJenlins(url=execute_environment['url'],username=execute_environment['username'],password=execute_environment['password'],param_dict =param_dict)
+    a.build_jenkinsJob(jobList)
 
-    # param_dict = {'Branch':'origin/fixbug-1.1.6','PublishVersion':'true'}
-    execute_environment = prodjenkins
-    # a = ContrlJenlins(url=execute_environment['url'],username=execute_environment['username'],password=execute_environment['password'],param_dict =param_dict)
-    # a.build_jenkinsJob(jobList)
 
-#同步最新
 
 
 
