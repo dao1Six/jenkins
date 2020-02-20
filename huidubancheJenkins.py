a7667651eef0726f95ebce389505c6e5b79f5fe3
wujianlun <905734003@qq.com>
2020-02-20 15:16:16 +0800

    修改

--- huidubancheJenkins.py
+++ huidubancheJenkins.py
@@ -2,10 +2,10 @@
 from controlJenkins import ContrlJenlins
 from excelcontrol import ExcelControl
 
-formaljenkins = {'url':'http://jenkins.qiqiao-prod.do1.work','username':'qiqiao','password':'6LypDvN2','param_dict':{}}
+formaljenkins = {'url':'http://jenkins.qiqiao-prod.do1.work','username':'qiqiao','password':'6LypDvN2'}
 
 #调个Jenkins控制器
-jc = ContrlJenlins(url=formaljenkins['url'],username=formaljenkins['username'],password=formaljenkins['password'],param_dict =formaljenkins['param_dict'])
+jc = ContrlJenlins(url=formaljenkins['url'],username=formaljenkins['username'],password=formaljenkins['password'])
 
 #调个excel控制器
 ec = ExcelControl()
@@ -17,12 +17,18 @@ sheet_name = "banche"
 #生成后端要构建的服务及参数
 houdaundic = ec.excle_generate_dict(filepath,sheet_name,0,1)
 
+
 #生成前端要构建的服务及参数
 qiandaundic = ec.excle_generate_dict(filepath,sheet_name,2,3)
 
 #合并字典
 dictMerged = dict(houdaundic, **qiandaundic)
 
+
+
+
 #构建服务
 for key,value in dictMerged.items():
-    jc.server.build_job (key, parameters={'DeployVersion':value})
+    parametersValue = {'DeployVersion':value}
+    print(parametersValue)
+    jc.server.build_job(key,param_dict=parametersValue)
