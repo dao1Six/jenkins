95cfffb7f87cec18377217633eee300c585fa3a7
wujianlun <905734003@qq.com>
2020-03-17 21:09:57 +0800

    修改

--- beta/huidubancheJenkins.py
+++ beta/huidubancheJenkins.py
@@ -10,7 +10,7 @@ sheet_name = "banche"
 #生成后端要构建的服务及参数
 houdaundic = ec.excle_generate_dict(filepath,sheet_name,0,1)
 #生成前端要构建的服务及参数
-qiandaundic = ec.excle_generate_dict(filepath,sheet_name,2,3)
+qiandaundic = ec.excle_generate_dict(filepath,sheet_name,3,4)
 #合并字典
 dictMerged = dict(houdaundic, **qiandaundic)
 print(dictMerged)
@@ -24,51 +24,13 @@ api_token='6LypDvN2'
 server=jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
 
 
-info = server.get_job_info(name='beta/backend/bpms-operation')
-print(info)
-
-server.build_job(name='beta/backend/bpms-operation',parameters={'DeployVersion': 'v20200225-160902'})
-
-
-
-
-
-
+#构建服务
+for key,value in dictMerged.items():
+    if key != "":
+        param_dict = dict()
+        param_dict['name'] = 'DeployVersion'
+        param_dict['value'] = value
+        server.build_job(key,parameters=param_dict)
+        print("已构建"+key)
 
 
-
-# formaljenkins = {'url':'http://jenkins.qiqiao-prod.do1.work','username':'qiqiao','password':'6LypDvN2'}
-
-
-
-# #调个excel控制器
-# ec = ExcelControl()
-#
-# #excel信息
-# filepath = "./huidubanche.xlsx"
-# sheet_name = "banche"
-#
-# #生成后端要构建的服务及参数
-# houdaundic = ec.excle_generate_dict(filepath,sheet_name,0,1)
-#
-#
-# #生成前端要构建的服务及参数
-# qiandaundic = ec.excle_generate_dict(filepath,sheet_name,2,3)
-#
-# #合并字典
-# dictMerged = dict(houdaundic, **qiandaundic)
-# print(dictMerged)
-#
-#
-# #调个Jenkins控制器
-#
-# server = jenkins.Jenkins (url=formaljenkins['url'],username=formaljenkins['username'],password=formaljenkins['password'], timeout=50)
-#
-#
-# #构建服务
-# # for key,value in dictMerged.items():
-# #     param_dict = dict()
-# #     param_dict['DeployVersion'] = value
-# #     a.build_job(key,parameters=param_dict)
-#
-# server.build_job('beta/backend/fdn-schema',{'DeployVersion': 'v20200220-151739'})
--- prod/bancheJenkins.py
+++ prod/bancheJenkins.py
@@ -1,34 +1,36 @@
 #生产环境jenkins 信息
 from controlJenkins import ContrlJenlins
 from excelcontrol import ExcelControl
-
-formaljenkins = {'url':'http://jenkins.qiqiao-prod.do1.work','username':'qiqiao','password':'6LypDvN2'}
-
-#调个Jenkins控制器
-jc = ContrlJenlins(url=formaljenkins['url'],username=formaljenkins['username'],password=formaljenkins['password'])
-
+import jenkins
 #调个excel控制器
 ec = ExcelControl()
-
-#excel信息
-filepath = "./banche.xlsx"
+# excel信息
+filepath = "../banche.xlsx"
 sheet_name = "banche"
-
 #生成后端要构建的服务及参数
 houdaundic = ec.excle_generate_dict(filepath,sheet_name,0,1)
-
-
 #生成前端要构建的服务及参数
-qiandaundic = ec.excle_generate_dict(filepath,sheet_name,2,3)
-
+qiandaundic = ec.excle_generate_dict(filepath,sheet_name,3,4)
 #合并字典
 dictMerged = dict(houdaundic, **qiandaundic)
+print(dictMerged)
 
+#定义用户的User Id 和 API Token，获取方式同上文
+jenkins_server_url='http://jenkins.qiqiao-prod.do1.work'
+user_id='qiqiao'
+api_token='6LypDvN2'
 
+#实例化jenkins对象，连接远程的jenkins master server
+server=jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
 
 
 #构建服务
 for key,value in dictMerged.items():
-    parametersValue = {'DeployVersion':value}
-    print(parametersValue)
-    jc.server.build_job(key,param_dict=parametersValue)
+    if key != "":
+        param_dict = dict()
+        param_dict['name'] = 'DeployVersion'
+        param_dict['value'] = value
+        server.build_job(key,parameters=param_dict)
+        print("已构建"+key)
+
+
