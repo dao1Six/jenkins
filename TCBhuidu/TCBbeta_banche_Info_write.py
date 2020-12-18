ddaacaf999ea2c082c6952c7ffaa184f96a1937f
wujianlun <905734003@qq.com>
2020-12-18 11:05:57 +0800

    aaa

---
+++ TCBhuidu/TCBbeta_banche_Info_write.py
@@ -0,0 +1,56 @@
+# 获取灰度最新待发布包的版本填写到班车表
+import time
+
+from controlJenkins import ContrlJenlins
+from excelcontrol import ExcelControl
+
+prodjenkins = {'url':'http://jenkins.qiqiao-prod.do1.work','username':'qiqiao','password':'6LypDvN2'}
+param_dict = {}
+
+filepath = "../TCBhuidubanche.xlsx"
+sheet_name = "banche"
+
+
+
+
+a = ContrlJenlins(url=prodjenkins['url'],username=prodjenkins['username'],password=prodjenkins['password'])
+e = ExcelControl()
+
+#更新版本库
+param_list = ['fdn-scheduler','official-mobile','official-web','fdn-schema', 'bpms-repository', 'fdn-storage', 'bpms-appstore', 'bpms-workbench', 'fdn-authorize',
+              'bpms-portal', 'bpms-operation', 'bpms-runtime', 'fdn-message', 'console-web', 'runtime-web',
+              'runtime-mobile', 'operation-web','fdn-scheduler']
+for i in param_list:
+    a.server.build_job ('other/SyncPublishVersion', parameters={'ModuleName': i, 'BuildTime': 'TODAY'})
+    time.sleep (2)
+    print (i)
+
+
+# time.sleep(10)
+jenkinsJobNames = []
+#获取班车jobname数据
+
+#后端job数据
+backend = e.get_sheet_col_info(xlsxName=filepath,sheet_name=sheet_name,col_number=0)
+banbenValueList = []
+#获取prodjenkins上服务对应最新版本值
+for i in backend:
+    banbenValue = a.get_jenkinsJob_info (i)['actions'][0]['parameterDefinitions'][0]['defaultParameterValue']['value']
+    banbenValueList.append(banbenValue)
+    #写入excel
+e.write_info_into_row(filename=filepath,datalist=banbenValueList,clo_number=1)
+
+
+#前端班车jobname数据
+frontend = e.get_sheet_col_info(xlsxName=filepath,sheet_name=sheet_name,col_number=3)
+frontendValueList = []
+#获取prodjenkins上服务对应最新版本值
+for i in frontend:
+    if i != "":
+        frontendValue = a.get_jenkinsJob_info (i)['actions'][0]['parameterDefinitions'][0]['defaultParameterValue']['value']
+        frontendValueList.append(frontendValue)
+    #写入excel
+e.write_info_into_row(filename=filepath,datalist=frontendValueList,clo_number=4)
+
+
+
---
+++ TCBhuidu/TCBhuidubancheJenkins.py
@@ -0,0 +1,40 @@
+#TCB灰度构建
+#生产环境jenkins 信息
+from datetime import time
+from time import strftime, localtime
+
+from excelcontrol import ExcelControl
+import jenkins
+# 调个excel控制器
+ec = ExcelControl()
+# excel信息
+filepath = "../TCBhuidubanche.xlsx"
+sheet_name = "banche"
+#生成后端要构建的服务及参数
+houdaundic = ec.excle_generate_dict(filepath,sheet_name,0,1)
+#生成前端要构建的服务及参数
+qiandaundic = ec.excle_generate_dict(filepath,sheet_name,3,4)
+#合并字典
+dictMerged = dict(houdaundic, **qiandaundic)
+print(dictMerged)
+
+#定义用户的User Id 和 API Token，获取方式同上文
+jenkins_server_url='http://jenkins.qiqiao-prod.do1.work'
+user_id='qiqiao'
+api_token='6LypDvN2'
+
+#实例化jenkins对象，连接远程的jenkins master server
+server=jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
+
+
+#构建服务
+for key,value in dictMerged.items():
+    if key != "":
+        param_dict = dict()
+        param_dict['name'] = 'DeployVersion'
+        param_dict['value'] = value
+        print ("TCB灰度已构建服务: " + key +"版本号为: "+ value+"  构建时间: "+strftime("%Y-%m-%d %H:%M:%S",localtime()))
+        server.build_job(key,parameters=param_dict)
+
+
+
